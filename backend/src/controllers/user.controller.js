828ebe0b9ce50e64a6635122c8fbe44d318e15d9
Shaurya <omshaurya941@gmail.com>
2025-12-29 14:55:16 +0530

    Refactor user authentication controllers; add error handling for signup and login processes; ensure proper token generation and response structure

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,10 +1,15 @@
+import mongoose from "mongoose";
 import { User } from "../models/user.model.js";
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import { ENV } from "../config/ENV.js";
+import {ApiResponse} from "../utils/ApiResponse.js"
 
+
+
+// Generate access and refresh token 
 const generateAccessAndRefreshToken = asyncHandler( async (userId)=> {
-    const user = User.findById(userId)
+    const user = await User.findById(userId)
 
     if(!user){
         throw new ApiError(400, "User with this id does not exist")
@@ -18,6 +23,7 @@ const generateAccessAndRefreshToken = asyncHandler( async (userId)=> {
     return {accessToken, refreshToken};
 })
 
+// signup controller
 const signup = asyncHandler( async (req, res)=> {
     const {email, fullName, password} = req.body
 
@@ -25,7 +31,11 @@ const signup = asyncHandler( async (req, res)=> {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existingUser = await User.find({email})
+    if(password.length<6) {
+          throw new ApiError(400, "Password should be minimum of 6 characters")
+    }
+
+    const existingUser = await User.findOne({email})
 
     if(existingUser){
         throw new ApiError(400, "User with email already exists")
@@ -43,7 +53,7 @@ const signup = asyncHandler( async (req, res)=> {
         throw new ApiError(500, "something went wrong while registering user")
     }
 
-    const {accessToken, refreshToken} = generateAccessAndRefreshToken()
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
 
     const option = {
         httpOnly: true,
@@ -55,5 +65,42 @@ const signup = asyncHandler( async (req, res)=> {
     .cookie("refreshToken", refreshToken, option)
     .json(
         new ApiResponse(200, "User created successfully !", user)
+    )
+})
+
+//login controller
+const login = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    if(!email) {
+        throw new ApiError(400, "Email is required")
+    }
+
+    const user = await User.findOne({email: email})
+
+    if(!user){
+        throw new ApiError(404, "Account with this email does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordValid(password)
+
+    if(!isPasswordValid) {
+        throw new ApiError(400, "Email or password is incorrect")
+    }
+
+    const loginUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+
+    const options = {
+        httpOnly: true,
+        secure: ENV.NODE_ENV!=="development"
+    }
+
+    res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200, "login successful", loginUser)
     )
 })
