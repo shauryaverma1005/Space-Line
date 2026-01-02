c8cc871d2490480a62327c74f4c9c46d7f9172fc
Shaurya <omshaurya941@gmail.com>
2026-01-02 13:17:49 +0530

    Created protected routes in app.js with loaders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,54 @@
-import React, { useEffect } from 'react'
-import Navbar from "./components/Navbar.jsx"
-import { Routes, Route } from 'react-router-dom'
-import HomePage from "./pages/HomePage.jsx"
-import LoginPage from "./pages/LoginPage.jsx"
-import ProfilePage from "./pages/ProfilePage.jsx"
-import SettingsPage from "./pages/SettingsPage.jsx"
-import SignupPage from "./pages/SignupPage.jsx"
-import {useAuthStore} from "./store/useAuthStore.js"
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { Loader } from "lucide-react";
 
-import './App.css'
+import "./App.css";
 function App() {
+  const { authProfile, checkAuth, isCheckingAuth } = useAuthStore();
 
-    const {authProfile, checkAuth} = useAuthStore()
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
-    useEffect(()=>{
-        checkAuth();
-    }, [checkAuth])
-    
+  if (isCheckingAuth && !authProfile) {
     return (
-        <>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<HomePage/>} />
-                <Route path="/signup" element={<SignupPage/>} />
-                <Route path="login" element={<LoginPage/>} />
-                <Route path="/settings" element={<SettingsPage/>} />
-                <Route path="/profile" element={<ProfilePage />} />
-            </Routes>
-        </>
-    )
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authProfile ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authProfile ? <SignupPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="login"
+          element={!authProfile ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authProfile ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
