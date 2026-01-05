9744ce8de2bd823428699a56497ea1b4fd78d6d2
Shaurya <omshaurya941@gmail.com>
2026-01-05 07:42:21 +0530

    updated app.js removed extra links from cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ import { errorHandler } from "./middlewares/errorHandler.middleware.js"
 const app = express()
 
 app.use(cors({
-    origin: [ENV.ORIGIN, "http://localhost:5173", "http://localhost:3000", "http://localhost:5000"],
+    origin: [ENV.ORIGIN, "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
