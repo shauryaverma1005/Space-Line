6669bf7308a5830df95b053f4f3858905dfef31e
Shaurya <omshaurya941@gmail.com>
2026-01-12 03:41:09 +0530

    backend bug fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://space-line-4.onrender.com/api/v1",
     withCredentials: true
 })
 
